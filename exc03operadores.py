6ca8c7e5250e75de8472cf0b80f0b98fff9ec9dc
Luiz Fernando de Jesus Oliveira <nando13702@gmail.com>
2026-01-18 12:12:57 -0300

    Melhoria no operador modulo
    Adicionei tratamento de erro para divisão por zero

--- exc03operadores.py
+++ exc03operadores.py
@@ -1,6 +1,11 @@
-n1=float(input("Digite o primeiro número:"))
-n2=float(input("Digite o segundo número:"))
-print(f"Soma:{n1+n2}")
-print(f"Diferença:{n1-n2}")
-print(f"Produto:{n1*n2}")
-print(f"Resto da Divisão:{n1%n2}")
+n1 = float(input("Digite o primeiro número: "))
+n2 = float(input("Digite o segundo número: "))
+
+print(f"Soma: {n1 + n2}")
+print(f"Diferença: {n1 - n2}")
+print(f"Produto: {n1 * n2}")
+
+if n2 != 0:
+    print(f"Resto da divisão: {n1 % n2}")
+else:
+    print("Não é possível calcular o resto da divisão por zero")
