187074b8b2a8b3aa7054e0f9111a5e9812568fe5
Luiz Fernando de Jesus Oliveira <nando13702@gmail.com>
2026-01-19 12:07:44 -0300

    criação de comparação

---
+++ exc06analiselogica.py
@@ -0,0 +1,8 @@
+N1=int(input("Digite o numero:"))
+N2=int(input("Digite o numero:"))
+if N1 > N2:
+    print("Número 1 é maior")
+elif N1 < N2:
+    print("Número 2 é maior")
+else:
+    print("São números iguais")    
